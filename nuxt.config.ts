3085dab0c01fddcdfd80515119259e65c5adae12
Jeremy Wiersma <jeremy.wiersma@procare.co.nz>
2022-11-24 02:19:53 +1300

    Exercise 01

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	modules: [
+    [
+      '@storyblok/nuxt',
+      {
+				accessToken: '65plU31QfQa3ScTzCxMMcgtt',
+				// If Server Location === US then:
+				apiOptions: { region: 'us' }
+			}
+		],
+		'@nuxtjs/tailwindcss'
+  ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+const story = await useAsyncStoryblok('home', { version: 'draft' }) // published
+</script>
+
+<template>
+	<StoryblokComponent v-if="story" :blok="story.content" />
+</template>
